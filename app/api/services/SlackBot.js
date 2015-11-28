7b066c1af2c6fc88b74dfd586a5c7d3929b15ae2
ferrante <damian.wielgosik@gmail.com>
2015-11-28 14:01:47 +0100

    Ladder

--- app/api/services/SlackBot.js
+++ app/api/services/SlackBot.js
@@ -9,6 +9,26 @@
     var text = message.text;
     var user = null;
 
+    if (/status/.test(text) && (new RegExp("@" + slack.self.id)).test(text)) {
+      var ladder = [];
+      for (var id in SlackUsers) {
+        if (SlackUsers.hasOwnProperty(id)) {
+          console.log(">>>>", SlackUsers[id]);
+          ladder.push(SlackUsers[id]);
+        }
+      }
+      ladder.sort(function (a, b) {
+        return a.getScore() - b.getScore();
+      });
+      var ladderText = "";
+      var place = 1;
+      ladder.forEach(function (user) {
+        ladderText += place + ". " + user.getUsername() + "(" + user.getScore() + "PLN)";
+      });
+      channel.send("Your current balance is " + user.getScore() + " PLN");
+      return;
+    }
+
     if (!SlackUsers.hasOwnProperty(slackUser.id)) {
       SlackUsers[slackUser.id] = user = SlackUser({
         slackUser: slackUser
